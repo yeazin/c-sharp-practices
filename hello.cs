a4e9e7373a2651cc1f5596cc2fa3e1531c8c9f98
Md. N. Islam Yeasin <naz.yeasin@gmail.com>
2022-07-31 14:15:34 +0600

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -1,1 +1,14 @@
+// C# Hello Project
 
+using System;
+
+namespace HelloWord
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the Earth");
+        }
+    }
+}
